1a6e2e4f20c6ead48ca3c7daa99a4f8c8e3300e1
lrobinson <lrobinson@gwmail.gwu.edu>
2016-01-18 03:47:11 +0000

    Write some submission message here

--- hello.c
+++ hello.c
@@ -7,5 +7,21 @@
 int main(int argc, char *argv[])
 {
 	printf("Hello world!\n");
+	printf("by Lauren Robinson\n");
+
+	/* This Section computes a sum*/
+	int x, y, z;
+	x = 10;
+	y = 20;
+	z = x + y;
+	printf("x is %d, y is %d, and z is %d\n", x, y, z);
+	/*this section calculates the volume of a box*/
+	int h, d, w, v;
+	h = 11;
+	d = 14;
+	w = 7;
+	v = h*d*w; 
+	printf("height is %d, depth is %d, width is %d, and volume is %d\n", h, d, w, v);
 	return 0;
+
 }
